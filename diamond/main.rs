d10759f633511f4dd4797d0d5de053ac00f75050
pallat <pallat@nongs-MacBook-Pro.local>
2021-06-21 10:52:58 +0700

    cast _n as usize too

--- diamond/main.rs
+++ diamond/main.rs
@@ -7,13 +7,14 @@ fn daimond(_n: i32) {
 
     for x in (0.._n).rev() {
         let x = x as usize;
-        println!("{}{}"," ".repeat(x), "*".repeat((_n as usize * 2)-(2 * x) -1));
-        
+        let _n = _n as usize;
+        println!("{}{}"," ".repeat(x), "*".repeat((_n * 2)-(2 * x) -1));
     }
     
     for x in 1.._n {
         let x = x as usize;
-        println!("{}{}"," ".repeat(x), "*".repeat((_n as usize * 2)-(2 * x) -1));
+        let _n = _n as usize;
+        println!("{}{}"," ".repeat(x), "*".repeat((_n * 2)-(2 * x) -1));
     }
 
 }
